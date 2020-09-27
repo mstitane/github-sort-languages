015954cfbaba6719e6b581b9d933db1516c6e781
mstitane <mohamed.stitane@opencellsoft.com>
2020-09-27 17:00:09 +0100

    initialising project structure

---
+++ src/main/java/com/stitane/githubsortlanguages/GithubSortLanguagesApplication.java
@@ -0,0 +1,13 @@
+package com.stitane.githubsortlanguages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSortLanguagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSortLanguagesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/stitane/githubsortlanguages/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.stitane.githubsortlanguages;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GithubSortLanguagesApplication.class);
+	}
+
+}
